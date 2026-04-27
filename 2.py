6fe6566c53cfb8023307e1453451a40c7341e7f9
jdx114514 <jdx114514@gmail.com>
2026-04-27 23:14:54 +0800

    88

---
+++ 2.py
@@ -0,0 +1,13 @@
+print(isinstance("hello world",str))
+print(type("hello world"))
+
+
+s1="我是" + "天才"
+print(s1)
+
+s2="努力"
+s3="勤奋"
+age=16
+print(type(age))  #加号两边必须同一类型,因此将 "age" 转换为str字符串
+print("我是"+s2 + s3,",年龄"+ str(age))
+
